d44a4cae3e5a0727a96ecc67174da1150bf72764
Daniel <790119+DanTheMan827@users.noreply.github.com>
2022-09-30 11:18:08 -0500

    Song packager and info generator

---
+++ tools/song-packager/song-packager/Extensions/MoggSong+LoadDta.cs
@@ -0,0 +1,121 @@
+﻿using DtxCS.DataTypes;
+
+namespace song_packager.Extensions
+{
+    public static class MoggSongExtensions
+    {
+        public static MoggSong LoadDta(this MoggSong moggSong, DataArray root)
+        {
+            var song = new MoggSong();
+
+            var dict = new Dictionary<string, string>();
+
+            foreach (var entry in root.Children.Where(x => x is DataArray).Cast<DataArray?>().Where(x => x is not null && x.Children.Count == 2 && x.Children[0] is DataSymbol && (x.Children[1] is DataAtom || x.Children[1] is DataSymbol)))
+            {
+                string? key = (entry!.Children[0] as DataSymbol)!.ToString();
+                var atom = entry.Children[1];
+                string? value = atom.ToString();
+
+                if (atom is DataAtom castedAtom && castedAtom.Type == DataType.STRING)
+                {
+                    value = castedAtom.String;
+                }
+
+                dict.Add(key, value!);
+            }
+
+            if (dict.ContainsKey("mogg_path"))
+            {
+                moggSong.MoggPath = dict["mogg_path"];
+            }
+
+            if (dict.ContainsKey("midi_path"))
+            {
+                moggSong.MidiPath = dict["midi_path"];
+            }
+
+            if (dict.ContainsKey("arena_path"))
+            {
+                moggSong.ArenaPath = dict["arena_path"];
+            }
+
+            if (dict.ContainsKey("tunnel_scale"))
+            {
+
+                if (decimal.TryParse(dict["tunnel_scale"], out decimal decValue))
+                {
+                    moggSong.TunnelScale = decValue;
+                }
+            }
+
+            if (dict.ContainsKey("title"))
+            {
+                moggSong.Title = dict["title"];
+            }
+
+            if (dict.ContainsKey("title_short"))
+            {
+                moggSong.ShortTitle = dict["title_short"];
+            }
+
+            if (dict.ContainsKey("artist"))
+            {
+                moggSong.Artist = dict["artist"];
+            }
+
+            if (dict.ContainsKey("artist_short"))
+            {
+                moggSong.ShortArtist = dict["artist_short"];
+            }
+
+            if (dict.ContainsKey("desc"))
+            {
+                moggSong.Description = dict["desc"];
+            }
+
+            if (dict.ContainsKey("unlock_requirement"))
+            {
+                moggSong.UnlockRequirement = dict["unlock_requirement"];
+            }
+
+            if (dict.ContainsKey("bpm"))
+            {
+
+                if (int.TryParse(dict["bpm"], out int intVal))
+                {
+                    moggSong.Bpm = intVal;
+                }
+            }
+
+            if (dict.ContainsKey("preview_start_ms"))
+            {
+
+                if (int.TryParse(dict["preview_start_ms"], out int intVal))
+                {
+                    moggSong.PreviewStartMs = intVal;
+                }
+            }
+
+            if (dict.ContainsKey("preview_length_ms"))
+            {
+
+                if (int.TryParse(dict["preview_length_ms"], out int intVal))
+                {
+                    moggSong.PreviewLengthMs = intVal;
+                }
+            }
+
+            if (dict.ContainsKey("demo_video"))
+            {
+                moggSong.DemoVideo = dict["demo_video"];
+            }
+
+            if (dict.ContainsKey("charter"))
+            {
+                moggSong.Charter = dict["charter"];
+            }
+
+            return moggSong;
+        }
+    }
+}
---
+++ tools/song-packager/song-packager/MoggSong.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace song_packager
+{
+    public class MoggSong
+    {
+        private static Regex cleanRegex = new Regex(@"\s+", RegexOptions.Compiled);
+        private static Regex nameArtistCleanRegex = new Regex(@"[^a-zA-Z\d\s~`!@#$%^&*()_\-+=[{\]}\\|<,>\./?:]+", RegexOptions.Compiled);
+        public string MoggPath { get; set; }
+        public string MidiPath { get; set; }
+        public string ArenaPath { get; set; }
+        public decimal TunnelScale { get; set; }
+        public string Title { get; set; }
+        public string CleanTitle { get => nameArtistCleanRegex.Replace(this.Title, ""); }
+        public string ShortTitle { get; set; }
+        public string Artist { get; set; }
+        public string CleanArtist { get => nameArtistCleanRegex.Replace(this.Artist, ""); }
+        public string ShortArtist { get; set; }
+        public string Description { get; set; }
+        public string CleanDescription => this.Description == null ? null : cleanRegex.Replace(this.Description, " ");
+        public string UnlockRequirement { get; set; }
+        public int Bpm { get; set; }
+        public int PreviewStartMs { get; set; }
+        public int PreviewLengthMs { get; set; }
+        public string DemoVideo { get; set; }
+        public string Charter { get; set; }
+    }
+}
